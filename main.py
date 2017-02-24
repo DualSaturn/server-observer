d1a91c80d4cefff651eb93ffa398d4a854555c94
DualSaturn <saturn031000@gmail.com>
2017-02-24 16:01:39 +0530

    Update main.py
    a much better approach which involves pulling the logs instead of storing them

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ client = discord.Client()
 
 @client.event
 async def on_message(message):
-    server_name = message.server.name
-    if 'server-observer, what do you see' in message.content.lower().strip(): #ignores the message if it has the activation message
-        with open('{}.txt'.format(server_name), 'r') as chat_log:
-            text = chat_log.read()
-            markgen = MarkovGenerator(text)
-            await client.send_message(message.channel, markgen.create_markov_chain_text(100))
-    elif message.author.bot is False: #ignores messages from bots
-        with open('{}.txt'.format(server_name), 'a') as chat_log:
-            chat_log.write('{}\n'.format(message.content))
+    if 'server-observer, what do you see' in message.content.lower():
+        chat_log_text = ''
+        async for msg in client.logs_from(message.channel, limit = 500):
+            if not msg.author.bot and 'server-observer, what do you see' not in msg.content.lower(): #ignores bots and messages with the activation phrase
+                chat_log_text += '{}\n'.format(msg.content)
+        markgen = MarkovGenerator(chat_log_text)
+        await client.send_message(message.channel, markgen.create_markov_chain_text(100))
 
 @client.event
 async def on_ready():
